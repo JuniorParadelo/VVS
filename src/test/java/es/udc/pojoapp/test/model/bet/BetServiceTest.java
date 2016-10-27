0a761fffbb974e1d1af161a837ab607e1f0a7cac
JuniorParadelo <juniorparadelo@gmail.com>
2016-10-27 18:11:43 +0200

    Test unidad bet

--- src/test/java/es/udc/pojoapp/test/model/bet/BetServiceTest.java
+++ src/test/java/es/udc/pojoapp/test/model/bet/BetServiceTest.java
@@ -1,19 +1,15 @@
 package es.udc.pojoapp.test.model.bet;
 
-
 import static es.udc.pojoapp.model.util.GlobalNames.SPRING_CONFIG_FILE;
 import static es.udc.pojoapp.test.util.GlobalNames.SPRING_CONFIG_TEST_FILE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -43,36 +39,35 @@ import es.udc.pojoapp.model.userservice.UserService;
 @ContextConfiguration(locations = { SPRING_CONFIG_FILE, SPRING_CONFIG_TEST_FILE })
 @Transactional
 public class BetServiceTest {
-		
+
 	@Autowired
 	private BetService betService;
-	
+
 	@Autowired
 	private BetTypeService optionService;
-	
+
 	@Autowired
 	private EventService eventService;
 
 	@Autowired
 	private UserService userService;
-	
+
 	private Category createCat() {
 		return eventService.createCategory("Futbol");
 	}
-	
+
 	private Event createEvent(Category cat) throws PastEventException, DuplicateEventException {
 		Calendar date = Calendar.getInstance();
 		date.add(Calendar.DAY_OF_MONTH, 2);
-		return eventService.createEvent("Deportivo - Real Madrid",date,cat.getIdentificador());
+		return eventService.createEvent("Deportivo - Real Madrid", date, cat.getIdentificador());
 	}
-	
+
 	private UserProfile createUser() throws DuplicateInstanceException {
-		return userService.registerUser("user", "password", 
-				new UserProfileDetails("name", "lname", "name@email.es"));
+		return userService.registerUser("user", "password", new UserProfileDetails("name", "lname", "name@email.es"));
 	}
-	
+
 	private List<Option> createoptions() {
-		Option option1 = new Option("Ronaldo", 1.05, null); 
+		Option option1 = new Option("Ronaldo", 1.05, null);
 		Option option2 = new Option("Benzema", 1.15, null);
 		Option option3 = new Option("Arbeloa", 11.05, null);
 		List<Option> options = new ArrayList<Option>();
@@ -81,85 +76,124 @@ public class BetServiceTest {
 		options.add(option3);
 		return options;
 	}
-	
+
 	private BetType createBetType(Event e) throws EventEmpezadoException, BetTypesIgualesException {
 		try {
 			BetType bet = new BetType("Jugador?", e, false);
 			List<Option> opts = createoptions();
 			Long id = e.getId();
-		optionService.create(bet,opts,id);
-		List<BetType> bets = new ArrayList<BetType>();
-		bets.addAll(e.getBetTypes());
-		for (int i=0; i<bets.size();i++) {
-			BetType b = (BetType) bets.get(i);
-			if (b.getQuestion().equals("Jugador?"))
-				return b;
-		}
+			optionService.create(bet, opts, id);
+			List<BetType> bets = new ArrayList<BetType>();
+			bets.addAll(e.getBetTypes());
+			for (int i = 0; i < bets.size(); i++) {
+				BetType b = (BetType) bets.get(i);
+				if (b.getQuestion().equals("Jugador?"))
+					return b;
+			}
 		} catch (InstanceNotFoundException e1) {
 			e1.printStackTrace();
 		}
 		return null;
 	}
-	
+
+	// PR-UN-022
 	@Test
-	public void testCreateBetAndFindBet() throws InstanceNotFoundException, DuplicateInstanceException, EventEmpezadoException, PastEventException, DuplicateEventException, BetTypesIgualesException {
+	public void testCreateBetAndFindBet() throws InstanceNotFoundException, DuplicateInstanceException,
+			EventEmpezadoException, PastEventException, DuplicateEventException, BetTypesIgualesException {
 
 		Category category = createCat();
 		Event event = createEvent(category);
 		BetType betType = createBetType(event);
 		UserProfile user = createUser();
 		Float apuesta = (float) 15;
-		Bet bet = betService.createBet(betType.getOptions().get(0).getId(),user.getUserProfileId(),apuesta);
+		Bet bet = betService.createBet(betType.getOptions().get(0).getId(), user.getUserProfileId(), apuesta);
 		Bet bet2 = betService.findBet(bet.getId());
-		
+
 		assertEquals(bet, bet2);
 	}
-	    
-    
-    @Test
-    public void testFindBetsByUserId() throws InstanceNotFoundException, DuplicateInstanceException, EventEmpezadoException, PastEventException, DuplicateEventException, BetTypesIgualesException{
-        
-        /* Create the accounts that should be found and store them in the 
-           correct order (we assume that the account identifiers are generated
-           in ascending order; in other case the list should be ordered by 
-           the account identifiers). */
-	    int numberOfBets = 11;
-	    List<Bet> expectedBets = new ArrayList<Bet>();       
-	    Category category = createCat();
+
+	// PR-UN-023
+	@Test
+	public void testFindBetsByUserId() throws InstanceNotFoundException, DuplicateInstanceException,
+			EventEmpezadoException, PastEventException, DuplicateEventException, BetTypesIgualesException {
+
+		/*
+		 * Create the accounts that should be found and store them in the
+		 * correct order (we assume that the account identifiers are generated
+		 * in ascending order; in other case the list should be ordered by the
+		 * account identifiers).
+		 */
+		int numberOfBets = 11;
+		List<Bet> expectedBets = new ArrayList<Bet>();
+		Category category = createCat();
 		Event event = createEvent(category);
 		BetType betType = createBetType(event);
 		UserProfile user = createUser();
-       for (int i = 0; i < numberOfBets; i++) {
-        	expectedBets.add(betService.createBet(betType.getOptions().get(0).getId(),
-        			user.getUserProfileId(),(float) 15.00));
-        }
-        
-       UserProfile user2 = userService.registerUser("user2", "pword", 
-				new UserProfileDetails("nae", "lnme2", "nme@email.es"));        
-        //Create an account that should not be found. 
-        betService.createBet(betType.getOptions().get(2).getId(),
-        		user2.getUserProfileId(), (float) 15.00);
-        
-        BetBlock betBlock;
-        int count = 10;
-        int startIndex = 0;
+		for (int i = 0; i < numberOfBets; i++) {
+			expectedBets.add(
+					betService.createBet(betType.getOptions().get(0).getId(), user.getUserProfileId(), (float) 15.00));
+		}
 
-        //Check account retrieval. 
-        short resultIndex = 0;
-        do {
-            betBlock = betService.findBetsByUserId(user.getUserProfileId(),
-                    startIndex, count);
-            assertTrue(betBlock.getBets().size() <= count);
-            for (Bet bet : betBlock.getBets()) {
-                assertTrue(expectedBets.contains(bet));
-            }
-            startIndex += count;
+		UserProfile user2 = userService.registerUser("user2", "pword",
+				new UserProfileDetails("nae", "lnme2", "nme@email.es"));
+		// Create an account that should not be found.
+		betService.createBet(betType.getOptions().get(2).getId(), user2.getUserProfileId(), (float) 15.00);
 
-        } while (betBlock.getExistMoreBets());
+		BetBlock betBlock;
+		int count = 10;
+		int startIndex = 0;
 
-        assertTrue(numberOfBets == startIndex - count
-                + betBlock.getBets().size());
+		// Check account retrieval.
+		short resultIndex = 0;
+		do {
+			betBlock = betService.findBetsByUserId(user.getUserProfileId(), startIndex, count);
+			assertTrue(betBlock.getBets().size() <= count);
+			for (Bet bet : betBlock.getBets()) {
+				assertTrue(expectedBets.contains(bet));
+			}
+			startIndex += count;
 
-    }
+		} while (betBlock.getExistMoreBets());
+
+		assertTrue(numberOfBets == startIndex - count + betBlock.getBets().size());
+
+	}
+
+	// PR-UN-024
+	@Test(expected = InstanceNotFoundException.class)
+	public void testCreateBetAndFindBet2() throws InstanceNotFoundException, DuplicateInstanceException,
+			EventEmpezadoException, PastEventException, DuplicateEventException, BetTypesIgualesException {
+
+		Bet bet = betService.createBet(123456789L, 3L, 6F);
+	}
+
+	// PR-UN-025
+	@Test(expected = InstanceNotFoundException.class)
+	public void testCreateBetAndFindBet3() throws InstanceNotFoundException, DuplicateInstanceException,
+			EventEmpezadoException, PastEventException, DuplicateEventException, BetTypesIgualesException {
+
+		Category category = createCat();
+		Event event = createEvent(category);
+		BetType betType = createBetType(event);
+		UserProfile user = createUser();
+		Float apuesta = (float) 15;
+		Bet bet = betService.createBet(betType.getOptions().get(0).getId(), 67L, apuesta);
+	}
+
+	// PR-UN-026
+	@Test(expected = EventEmpezadoException.class)
+	public void testCreateBetAndFindBet4() throws InstanceNotFoundException, DuplicateInstanceException,
+			EventEmpezadoException, PastEventException, DuplicateEventException, BetTypesIgualesException {
+
+		Category category = createCat();
+		Calendar date = Calendar.getInstance();
+		date.add(Calendar.DAY_OF_MONTH, -5);
+		Event event = eventService.createEvent("Deportivo - Real Madrid", date, category.getIdentificador());
+		BetType betType = createBetType(event);
+		UserProfile user = createUser();
+		Float apuesta = (float) 15;
+		Bet bet = betService.createBet(betType.getOptions().get(0).getId(), user.getUserProfileId(), apuesta);
+
+	}
+
 }
-
--- src/test/java/es/udc/pojoapp/test/model/bet/BetTypeServiceTest.java
+++ src/test/java/es/udc/pojoapp/test/model/bet/BetTypeServiceTest.java
@@ -2,7 +2,9 @@ package es.udc.pojoapp.test.model.bet;
 
 import static es.udc.pojoapp.model.util.GlobalNames.SPRING_CONFIG_FILE;
 import static es.udc.pojoapp.test.util.GlobalNames.SPRING_CONFIG_TEST_FILE;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -30,142 +32,157 @@ import es.udc.pojoapp.model.eventservice.EventService;
 import es.udc.pojoapp.model.eventservice.PastEventException;
 import es.udc.pojoapp.model.option.Option;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { SPRING_CONFIG_FILE, SPRING_CONFIG_TEST_FILE })
 @Transactional
 public class BetTypeServiceTest {
-	
+
 	@Autowired
 	private EventService eventService;
-	
+
 	@Autowired
 	private BetTypeService betService;
-	
-	
+
 	private Category createCat(String name) {
 		return eventService.createCategory(name);
 	}
-	
+
 	private Event defaultEvent(Category cat) throws PastEventException, DuplicateEventException {
 		Calendar date = Calendar.getInstance();
-		date.add(Calendar.DAY_OF_MONTH,11);
-		return eventService.createEvent("Deportivo - Real Madrid",date,cat.getIdentificador());
+		date.add(Calendar.DAY_OF_MONTH, 11);
+		return eventService.createEvent("Deportivo - Real Madrid", date, cat.getIdentificador());
 	}
-	
-	private BetType createBetType(String question, Event event){
-		return new BetType(question, event,false);
+
+	private BetType createBetType(String question, Event event) {
+		return new BetType(question, event, false);
 	}
-	
-	private Option createOption(String name,BetType betType){
+
+	private Option createOption(String name, BetType betType) {
 		return new Option(name, 1.5, betType);
 	}
-	
+
+	// PR_UN-029
 	@Test(expected = BetTypesIgualesException.class)
 	@Transactional
-	public void createOpcionesDeApuestaTest1() throws InstanceNotFoundException, EventEmpezadoException, PastEventException, DuplicateEventException, BetTypesIgualesException{
+	public void createOpcionesDeApuestaTest1() throws InstanceNotFoundException, EventEmpezadoException,
+			PastEventException, DuplicateEventException, BetTypesIgualesException {
 		Event evento = defaultEvent(createCat("Futbol"));
-		BetType bet = createBetType("Ganador?",evento);
+		BetType bet = createBetType("Ganador?", evento);
 		Option opcion1 = createOption("Deportivo", bet);
 		Option opcion2 = createOption("Real Madrid", bet);
 		List<Option> options = new ArrayList<Option>();
-		options.add(opcion1);  options.add(opcion2);
+		options.add(opcion1);
+		options.add(opcion2);
 		betService.create(bet, options, evento.getId());
-		
+
 		betService.create(bet, options, evento.getId());
 	}
-	
-	
+
+	// PR_UN-030
 	@Test(expected = EventEmpezadoException.class)
 	@Transactional
-	public void createOpcionesDeApuestaTest() throws InstanceNotFoundException, EventEmpezadoException, PastEventException, DuplicateEventException, BetTypesIgualesException{
+	public void createOpcionesDeApuestaTest() throws InstanceNotFoundException, EventEmpezadoException,
+			PastEventException, DuplicateEventException, BetTypesIgualesException {
 		Event evento = defaultEvent(createCat("Futbol"));
 		evento.getDate().set(Calendar.YEAR, 2014);
-		BetType bet = createBetType("Ganador?",evento);
+		BetType bet = createBetType("Ganador?", evento);
 		Option opcion1 = createOption("Deportivo", bet);
 		Option opcion2 = createOption("Real Madrid", bet);
 		List<Option> options = new ArrayList<Option>();
-		options.add(opcion1);  options.add(opcion2);
+		options.add(opcion1);
+		options.add(opcion2);
 		betService.create(bet, options, evento.getId());
 	}
-	
-	
+
+	// PR_UN-027
+	// PR_UN-028
 	@Test
 	@Transactional
-	public void createOptionesDeApuestaTest()throws InstanceNotFoundException, EventEmpezadoException, PastEventException, DuplicateEventException, BetTypesIgualesException{
+	public void createOptionesDeApuestaTest() throws InstanceNotFoundException, EventEmpezadoException,
+			PastEventException, DuplicateEventException, BetTypesIgualesException {
 
 		Event evento = defaultEvent(createCat("Futbol"));
-		BetType bet = createBetType("Ganador?",evento);
+		BetType bet = createBetType("Ganador?", evento);
 		Option opcion1 = createOption("Deportivo", bet);
 		Option opcion2 = createOption("Real Madrid", bet);
 		List<Option> options = new ArrayList<Option>();
-		options.add(opcion1);  options.add(opcion2);
+		options.add(opcion1);
+		options.add(opcion2);
 		betService.create(bet, options, evento.getId());
-		
-		//COMPROBACION DE DATOS
+
+		// COMPROBACION DE DATOS
 		Event eventoTest = eventService.findEvent(evento.getId());
 		BetType betTest = betService.findBetType(bet.getId());
 		assertTrue(eventoTest.getBetTypes().contains(betTest));
 		assertTrue(betTest.getOptions().containsAll(options));
-		
-		assertEquals(betTest.getOptions().size(),2);
-		
+
+		assertEquals(betTest.getOptions().size(), 2);
+
 	}
-	
-	
-	@Test (expected = UnicaGanadoraException.class )
+
+	// PR_UN-032
+	@Test(expected = UnicaGanadoraException.class)
 	@Transactional
-	public void actualizarTest3() throws InstanceNotFoundException, EventEmpezadoException, EventoNoEmpezadoException, OpcionesActualizadasException, PastEventException, DuplicateEventException, BetTypesIgualesException, UnicaGanadoraException{
+	public void actualizarTest3() throws InstanceNotFoundException, EventEmpezadoException, EventoNoEmpezadoException,
+			OpcionesActualizadasException, PastEventException, DuplicateEventException, BetTypesIgualesException,
+			UnicaGanadoraException {
 		Event evento = defaultEvent(createCat("Baloncesto"));
-		BetType bet = createBetType("Ganador?",evento);
+		BetType bet = createBetType("Ganador?", evento);
 		bet.setUnica(true);
 		Option opcion1 = createOption("Deportivo", bet);
 		Option opcion2 = createOption("Real Madrid", bet);
 		List<Option> options = new ArrayList<Option>();
-		options.add(opcion1);  options.add(opcion2);
+		options.add(opcion1);
+		options.add(opcion2);
 		betService.create(bet, options, evento.getId());
 		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DAY_OF_MONTH,-2);
+		cal.add(Calendar.DAY_OF_MONTH, -2);
 		evento.setDate(cal);
 		List<Long> optionIds = new ArrayList<Long>();
 		optionIds.add(opcion1.getId());
 		optionIds.add(opcion2.getId());
 		betService.updateState(bet.getId(), optionIds);
 	}
-	
-	
+
+	// PR_UN-031
 	@Test
 	@Transactional
-	public void actualizarTest1() throws InstanceNotFoundException, EventEmpezadoException, EventoNoEmpezadoException, OpcionesActualizadasException, PastEventException, DuplicateEventException, BetTypesIgualesException, UnicaGanadoraException{
+	public void actualizarTest1() throws InstanceNotFoundException, EventEmpezadoException, EventoNoEmpezadoException,
+			OpcionesActualizadasException, PastEventException, DuplicateEventException, BetTypesIgualesException,
+			UnicaGanadoraException {
 		Event evento = defaultEvent(createCat("Baloncesto"));
-		
-		
-		BetType bet = createBetType("Ganador?",evento);
+
+		BetType bet = createBetType("Ganador?", evento);
 		Option opcion1 = createOption("Deportivo", bet);
 		Option opcion2 = createOption("Real Madrid", bet);
 		List<Option> options = new ArrayList<Option>();
-		options.add(opcion1);  options.add(opcion2);
+		options.add(opcion1);
+		options.add(opcion2);
 		betService.create(bet, options, evento.getId());
 		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DAY_OF_MONTH,-2);
+		cal.add(Calendar.DAY_OF_MONTH, -2);
 		evento.setDate(cal);
 		List<Long> optionIds = new ArrayList<Long>();
 		optionIds.add(opcion1.getId());
 		betService.updateState(bet.getId(), optionIds);
 	}
-	
-	@Test (expected = OpcionesActualizadasException.class)
+
+	// PR_UN-033
+	@Test(expected = OpcionesActualizadasException.class)
 	@Transactional
-	public void actualizarTest2() throws InstanceNotFoundException, EventoNoEmpezadoException, OpcionesActualizadasException, EventEmpezadoException, PastEventException, DuplicateEventException, BetTypesIgualesException, UnicaGanadoraException{
+	public void actualizarTest2() throws InstanceNotFoundException, EventoNoEmpezadoException,
+			OpcionesActualizadasException, EventEmpezadoException, PastEventException, DuplicateEventException,
+			BetTypesIgualesException, UnicaGanadoraException {
 		Event evento = defaultEvent(createCat("Baloncesto"));
-		BetType bet = createBetType("Ganador?",evento);
+		BetType bet = createBetType("Ganador?", evento);
 		Option opcion1 = createOption("Deportivo", bet);
 		Option opcion2 = createOption("Real Madrid", bet);
 		List<Option> options = new ArrayList<Option>();
-		options.add(opcion1);  options.add(opcion2);
+		options.add(opcion1);
+		options.add(opcion2);
 		betService.create(bet, options, evento.getId());
 		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DAY_OF_MONTH,-2);
+		cal.add(Calendar.DAY_OF_MONTH, -2);
 		evento.setDate(cal);
 		List<Long> optionIds = new ArrayList<Long>();
 		optionIds.add(opcion1.getId());
@@ -179,40 +196,43 @@ public class BetTypeServiceTest {
 		betService.updateState(bet.getId(), optionIds);
 		System.out.println(opcion1.getVersion());
 	}
-	
+
+	// PR_UN-031
 	@Test
 	@Transactional
-	public void actualizarTest() throws InstanceNotFoundException, EventEmpezadoException, EventoNoEmpezadoException, OpcionesActualizadasException, PastEventException, DuplicateEventException, BetTypesIgualesException, UnicaGanadoraException{
+	public void actualizarTest() throws InstanceNotFoundException, EventEmpezadoException, EventoNoEmpezadoException,
+			OpcionesActualizadasException, PastEventException, DuplicateEventException, BetTypesIgualesException,
+			UnicaGanadoraException {
 
 		Event evento = defaultEvent(createCat("Baloncesto"));
-		BetType bet = createBetType("Ganador?",evento);
+		BetType bet = createBetType("Ganador?", evento);
 		Option opcion1 = createOption("Deportivo", bet);
 		Option opcion2 = createOption("Real Madrid", bet);
 		List<Option> options = new ArrayList<Option>();
-		options.add(opcion1);  options.add(opcion2);
+		options.add(opcion1);
+		options.add(opcion2);
 		betService.create(bet, options, evento.getId());
 		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DAY_OF_MONTH,-2);
+		cal.add(Calendar.DAY_OF_MONTH, -2);
 		evento.setDate(cal);
 		List<Long> optionIds = new ArrayList<Long>();
 		optionIds.add(opcion1.getId());
 		betService.updateState(bet.getId(), optionIds);
-		
-		//COMPROBACIONES
-		
+
+		// COMPROBACIONES
+
 		BetType betTest = betService.findBetType(bet.getId());
-		
+
 		List<Option> optionTest = betTest.getOptions();
-		
-		
-		if (optionTest.get(0).getId()==optionIds.get(0)){
+
+		if (optionTest.get(0).getId() == optionIds.get(0)) {
 			assertTrue(optionTest.get(0).getWin());
-		} else{
+		} else {
 			assertFalse(optionTest.get(0).getWin());
 		}
-		if (optionTest.get(1).getId()==optionIds.get(0)){
+		if (optionTest.get(1).getId() == optionIds.get(0)) {
 			assertTrue(optionTest.get(1).getWin());
-		} else{
+		} else {
 			assertFalse(optionTest.get(1).getWin());
 		}
 	}
